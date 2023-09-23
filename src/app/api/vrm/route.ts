92fa02cbd3637c9c5784af47d32d0c4df297e027
Jasper Degens <jasperdegens@gmail.com>
2023-09-23 15:14:41 -0400

    need redeploy so updated errors

--- src/app/api/vrm/route.ts
+++ src/app/api/vrm/route.ts
@@ -9,21 +9,21 @@ export async function GET(request: Request): Promise<NextResponse> {
   const token = cookies().get("mona-token-gater")?.value
   // check if token is valid
   if(!token) {
-    return new NextResponse("No token provided", {status: 400})
+    return new NextResponse("No token provided", {status: 401})
   }
 
   try {
     // @ts-ignore
     const validToken = jwt.decode(token, process.env.JWT_SECRET)
     if(!validToken || validToken.avatarId === undefined ) {
-      return new NextResponse("No Valid Avatar Id", {status: 400})
+      return new NextResponse("No Valid Avatar Id", {status: 402})
     }
     return new NextResponse(validToken.avatarId, {status: 200})
   }
   catch(e) {
     console.log(e)
   }
-  return new NextResponse("Invalid token", {status: 400})
+  return new NextResponse("Invalid token", {status: 403})
   
   
 
