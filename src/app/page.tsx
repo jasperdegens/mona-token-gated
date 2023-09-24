43151362da4a65886491c2be372df52394a160e3
Jasper Degens <jasperdegens@gmail.com>
2023-09-24 01:05:28 -0400

    added revoked UX

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image'
 
 export default function Home() {
 
-  const [authorizingState, setAuthorizingState] = useState<'NONE' | 'AUTHORIZING' | 'AUTHORIZED' | 'ERROR'>('NONE')
+  const [authorizingState, setAuthorizingState] = useState<'NONE' | 'AUTHORIZING' | 'AUTHORIZED' | 'ERROR' | 'REVOKED'>('NONE')
   const [authResponse, setAuthResponse] = useState<{avatarIds: string[], ip: string} | undefined>(undefined)
 
   const authorizeIP = useCallback(async () => {
@@ -22,6 +22,8 @@ export default function Home() {
 
   const revoke = async () => {
     await fetch('/api/authorize?revoke=true')
+    setAuthorizingState('REVOKED')
+    setAuthResponse(undefined)
   }
 
 
@@ -31,12 +33,16 @@ export default function Home() {
     <div className='w-full h-screen flex justify-center items-center bg-[#3d5e58]'>
       <div className="bg-white rounded-lg shadow p-12 space-y-6 max-w-lg -full">
         <h1 className="text-2xl">Welcome to the MonaGater!</h1>
-        {!authResponse && (<><p>This service is a propotype for token gating content within Mona virtual spaces. Currently, we use your IP address to assign authorized avatars, but in the future would love to use your wallet address so we can validate ownership of specific NFTs in targeted collections.</p>
+        {!authResponse && authorizingState !== 'REVOKED' &&  (<><p>This service is a propotype for token gating content within Mona virtual spaces. Currently, we use your IP address to assign authorized avatars, but in the future would love to use your wallet address so we can validate ownership of specific NFTs in targeted collections.</p>
         <p>To try it out, press the authorize IP button below, where you will be assigned several avatars you can access. Then enter the Mona playground, and swap the avatars freely!</p></>)}
         {authResponse && <div className='mt-4'>
         <div className='text-lg font-bold'>{`Your IP ${authResponse.ip} is Authorized!`}</div>
-        <div className='text-sm'>{`Authorized avatars are: ${authResponse.avatarIds.join(", ")}`}</div>
+        <div className='text-sm'>{`Authorized avatars are: ${authResponse.avatarIds.join(", ")}`}
+        </div>
       </div>}
+        {authorizingState === 'REVOKED' && <div className='mt-4'>
+        <div className='text-lg font-bold'>IP Revoked!</div>
+        </div>}
         <div className="w-full mx-auto text-center">
       <button
         className="px-8 py-4 bg-[#3d5e58] text-white font-bold rounded-lg shadow-lg hover:bg-green-800 w-full max-w-sm"
@@ -44,7 +50,7 @@ export default function Home() {
           authorizeIP()
         }}
         disabled={authorizingState === 'AUTHORIZING'}
-        >{authorizingState === 'NONE' ? 'Authorize IP' : authorizingState === 'AUTHORIZING' ? 'Authrizing...' : authorizingState == 'AUTHORIZED' ? 'Authorized' : 'Error'}
+        >{authorizingState === 'NONE' || authorizingState === 'REVOKED' ? 'Authorize IP' : authorizingState === 'AUTHORIZING' ? 'Authrizing...' : authorizingState == 'AUTHORIZED' ? 'Authorized' : 'Error'}
       </button> 
       <button
         className="mt-4 px-8 py-4 bg-red-700 text-white font-bold rounded-lg shadow-lg hover:bg-red-800 w-full max-w-sm"
